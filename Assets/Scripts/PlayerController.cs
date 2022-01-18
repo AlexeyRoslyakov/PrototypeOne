c950b289b29f2b7bc3eea171fec331f078a78101
AlexeyRoslyakov <Advs.air@gmail.com>
2022-01-19 02:25:25 +0300

    v0.2
    in this branch i test a new control with wheel colliders

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,28 +2,76 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor; //is this wheel attached to motor?
+    public bool steering; // does this wheel apply steer angle?
+}
+
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 20.0f;
-    private float turnSpeed = 45.0f;
-    private float horizontalInput;
-    private float forwardInput;
+
+    public List<AxleInfo> axleInfos; //the information about each individual axle
+    public float maxMotorTorque; // maximum torque  the motor can apply to wheel
+    public float maxSteeringAngle; // maximum steer angle the wheel can have
+
+
+    private Rigidbody playerRb;
+    [SerializeField] GameObject centerOfMass;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.centerOfMass = centerOfMass.transform.position;
     }
+
+    // find the corresponding visual wheel
+    // correctly applies the transform
+    public void ApplyLocalPositionToVisuals(WheelCollider collider)
+    {
+        if (collider.transform.childCount == 0)
+        {
+            return;
+        }
+
+        Transform visualWheel = collider.transform.GetChild(0);
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
+
+        visualWheel.transform.position = position;
+        visualWheel.transform.rotation = rotation;
+    }
+
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
 
-        //move car forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        //rotate car
-        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+
+            ApplyLocalPositionToVisuals(axleInfo.leftWheel);
+            ApplyLocalPositionToVisuals(axleInfo.rightWheel);
+        }
+
     }
+
+
 }
